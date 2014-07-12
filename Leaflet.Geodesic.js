dd7379232c824cc8551846bfd43ff5421d79678a
Henry Thasler <git@thasler.org>
2014-07-12 10:22:36 +0200

    class basics and options added

--- Leaflet.Geodesic.js
+++ Leaflet.Geodesic.js
@@ -1,10 +1,31 @@
 L.Geodesic = L.MultiPolyline.extend({
-  initialize: function(options) {
-	  
-  }
+    options: {
+	color:'blue'
+    },  
+  
+    initialize: function(latlngs, options) {
+      this.options = this._merge_options(this.options, options);
+      L.MultiPolygon.prototype.initialize.call(this, latlngs, this.options);    
+    },
+  
+    setLatLngs: function (latlngs) {
+      L.MultiPolygon.prototype.setLatLngs.call(this, latlngs);
+    },  
 
-});	
-	
-L.geodesic = function(options) {
-    return new L.Geodesic(options);
+  /**
+  * Overwrites obj1's values with obj2's and adds obj2's if non existent in obj1
+  * @param obj1
+  * @param obj2
+  * @returns obj3 a new object based on obj1 and obj2
+  */
+  _merge_options: function(obj1,obj2){
+      var obj3 = {};
+      for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+      for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+      return obj3;
+  }
+});
+
+L.geodesic = function(latlngs, options) {
+    return new L.Geodesic(latlngs, options);
 };
