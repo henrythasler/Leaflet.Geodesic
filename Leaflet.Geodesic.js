779d36684c8a76188f266ea34928de0c0b8fbefa
Henry <git@thasler.org>
2014-11-16 18:38:29 +0100

    revert

--- Leaflet.Geodesic.js
+++ Leaflet.Geodesic.js
@@ -49,10 +49,6 @@ L.Geodesic = L.MultiPolyline.extend({
       L.MultiPolyline.prototype.setLatLngs.call(this, this._latlngs);
     },
 
-    getLatLngs: function (latlngs) {
-      return L.MultiPolyline.prototype.getLatLngs.call(this);
-    },
-    
     /**
     * Calculates some statistic values of current geodesic multipolyline
     * @returns (Object} Object with several properties (e.g. overall distance)
