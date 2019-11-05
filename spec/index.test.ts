d77b2516fbf0fe06f05f8bc18ab7205259603e8c
henrythasler <git@thasler.org>
2019-11-05 15:38:23 +0100

    fix another naming error

--- spec/index.test.ts
+++ spec/index.test.ts
@@ -12,14 +12,14 @@ import "jest";
 describe("Object creation", function () {
     it("Classes", function () {
         const geodesic:L.Geodesic = new L.Geodesic();
-        const circle:L.GreatCircle = new L.GeodesicCircle();
+        const circle:L.GeodesicCircle = new L.GeodesicCircle();
         expect(geodesic).to.be.instanceOf(L.Geodesic);
         expect(circle).to.be.instanceOf(L.GeodesicCircle);
     });
 
     it("Functions", function () {
         const geodesic:L.Geodesic = L.geodesic();
-        const circle:L.GreatCircle = L.geodesiccircle();
+        const circle:L.GeodesicCircle = L.geodesiccircle();
         expect(geodesic).to.be.instanceOf(L.Geodesic);
         expect(circle).to.be.instanceOf(L.GeodesicCircle);
     });    
